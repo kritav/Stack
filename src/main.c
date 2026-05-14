ae263aecb9565f0419eac4e3666fb1d4b20a403d
kritav <51726974+kritav@users.noreply.github.com>
2026-05-14 10:20:37 -0700

    Variables updated

--- src/main.c
+++ src/main.c
@@ -7,15 +7,32 @@
 
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 240
-void FillScreen(uint8_t color);
+#define MAX_HEIGHT 30
+
+typedef struct {
+    int x;
+    int width;
+    uint8_t color;
+} Layer;
+
+Layer layers[MAX_HEIGHT];
+int num_layers;
+int direction;
+int game_over;
+
+void draw_block (int x, int y, int width, uint8_t color) {
+    gfx_SetColor(color);
+    gfx_FillRectangle(x, y, width, 15);
+}
 
 /* Main function, called first */
 int main(void) {
     gfx_Begin();
     gfx_SetDrawBuffer();
-    gfx_FillScreen(0x15);
-    gfx_SetColor(0xe0);
+    gfx_FillScreen(0x15); // light blue
+    gfx_SetColor(0xe0); // red
     gfx_FillRectangle(110, 200, 100, 15);
+
     gfx_BlitBuffer();
 
 
