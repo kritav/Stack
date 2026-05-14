8e98b2855f061b14b5308cac6f3c755caf9c30c1
kritav <51726974+kritav@users.noreply.github.com>
2026-05-14 10:08:40 -0700

    Width/height

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
+#include <keypadc.h>
 #include <stdlib.h>
 #include <sys/lcd.h>
 #include <graphx.h>
 #include <ti/getcsc.h>
 #include <ti/screen.h>
 
-
+#define SCREEN_WIDTH 320
+#define SCREEN_HEIGHT 240
 void FillScreen(uint8_t color);
 
 /* Main function, called first */
@@ -12,7 +14,13 @@ int main(void) {
     gfx_Begin();
     gfx_SetDrawBuffer();
     gfx_FillScreen(0x15);
+    gfx_SetColor(0xe0);
+    gfx_FillRectangle(110, 200, 100, 15);
     gfx_BlitBuffer();
+
+
+
+
     while (!os_GetCSC());
 
     gfx_End();
